7bdc6405d7745ebcc7b430905da4b93b61a5b836
thefuzz25 <104623695+thefuzz25@users.noreply.github.com>
2022-06-11 16:34:10 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,5 +17,20 @@ const linksToggleMenu = () => {
 }
 window.addEventListener('click', linksToggleMenu);
 //! === SWIPER ===
-
+const swiper = new Swiper('.myswiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidePerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 200,
+    modifier: 1;
+    slideShadows: true
+  },
+  pagination: {
+    el: '.swiper-pagination'
+  }
+});
 //! === SCROLL REVEAL ===
