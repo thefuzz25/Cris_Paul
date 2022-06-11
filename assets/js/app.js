ec3a1684162e66fd01f18e350189be4b3b9d17bd
thefuzz25 <104623695+thefuzz25@users.noreply.github.com>
2022-06-11 12:05:10 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 //! === ADD HEADER STYLES ON SCROLL ===
-
+const headerScroll = () => {
+  const headerElement = document.querySelector('.header');
+  this.scrollY >= 30 ? headerElement.classList.add('active') : headerElement.classList.remove('active');
+}
+window.addEventListener('scroll', headerScroll);
 //! === OPEN AND CLOSE THE MENU ON HAMBURGER ICON CLICK ===
 
 // --- CLOSE MENU WHEN NAV-LINKS ARE CLICKED ---
